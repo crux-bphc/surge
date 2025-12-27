2d64429633d9462f7676209da20975c5156e32b7
aten2005 <gogo.june2005@gmail.com>
2025-12-27 22:55:19 +0530

    feat(db): generated migrations for wrapped 25 table

---
+++ backend/src/drizzle/0001_create_wrapped_25_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "wrapped_25" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"submission_count" integer NOT NULL,
+	"solved_count" integer NOT NULL,
+	"monthly_solves" jsonb NOT NULL,
+	"accuracy" real NOT NULL,
+	"most_solved_tags" jsonb NOT NULL,
+	"longest_streak" integer DEFAULT 1 NOT NULL,
+	"contest_count" integer DEFAULT 0 NOT NULL,
+	"initial_rating" integer,
+	"final_rating" integer,
+	"potd_solves" integer,
+	"campus_rank" integer NOT NULL,
+	"batch_rank" integer NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "wrapped_25" ADD CONSTRAINT "wrapped_25_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
